47d0c00967aece8311fff084e08246977548bb58
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-20 00:07:30 +0300

    fix linters

--- app/models/food.rb
+++ app/models/food.rb
@@ -3,7 +3,6 @@ class Food < ApplicationRecord
   validates :measurement_unit, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,6 @@ class Recipe < ApplicationRecord
   validates :preperation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :description, presence: true
-
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
 end
