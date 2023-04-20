a931a4e707ffa69dbbb1abc14b9f50b117f4ecf1
Faranosh Amini <faranosh.amini4@gmail.com>
2023-04-20 22:35:24 +0430

    Create foods_controller.rb

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,39 @@
+class FoodsControll < ApplicationController
+  def index
+    @foods = current_user.foods.includes(:user).all
+  end
+  
+  def show
+    @food = current_user.foods.includes(:user).find(params[:id])
+    @inventories = Inventory.all
+  end
+  
+  def new
+    @food = Food.new
+  end 
+  
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+    if @food.save
+       flash[:success] = 'New Food has been added !!'
+       redirect_to foods_path
+    else
+       flash['alert'] = 'Food not added, try again !!'
+    render :New
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    flash[:success] = 'Food has beed deleted successfully'
+    redirect_to foods_path
+  end
+  
+  private
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price)
+  end  
+end    
