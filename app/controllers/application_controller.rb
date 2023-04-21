273654b86ed04da1b459dcbd9d601e1832086321
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-21 04:13:38 +0300

    Impelemet Recipe List

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(_resource)
-    users_index_url
+    user_recipes_path
   end
 
   protected
