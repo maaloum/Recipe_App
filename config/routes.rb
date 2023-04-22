b638b178bc13518001599b53c25dec391e2396a2
Faranosh Amini <faranosh.amini4@gmail.com>
2023-04-22 21:00:56 +0430

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'recipes#index'
   resources :users, only: [:index, :show] do
+    resources :foods, except: [:update] 
     resources :recipes, except: [:update] 
     resources :general_shopping_list, only: [:index]
   end
