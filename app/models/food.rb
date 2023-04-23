6f2999dc948063e8b0ac2ec26f999e509a0e147f
Maaloum <50062179+maaloum@users.noreply.github.com>
2023-04-23 22:19:40 +0300

    fix food delete

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,11 +1,9 @@
 class Food < ApplicationRecord
-  validates :name, presence: true, length: { maximum: 255 }
+  validates :name, presence: true
   validates :measurement_unit, presence: true
-  validates :price, presence: true, comparison: { greater_than_or_equal_to: 0 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-  has_many :inventory_foods, dependent: :destroy
-  has_many :inventories, through: :inventory_foods, dependent: :destroy
-  has_many :recipes, through: :recipe_food, dependent: :destroy
 end
