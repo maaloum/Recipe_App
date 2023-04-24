0ebece6c7421f9e28690b15a2f6f3d84ec45db26
Faranosh Amini <faranosh.amini4@gmail.com>
2023-04-24 03:31:46 -0700

    Fix rubocop errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(_resource)
     user_foods_path(current_user.id)
   end
-  def after_sign_out_path_for(resource)
+  
+  def after_sign_out_path_for(*)
     new_user_session_path
   end
 
